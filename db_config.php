b7c4ed156f728f5fe3ce6bb89f035e87e96d6dcf
Saltiberra <36106278+Saltiberra@users.noreply.github.com>
2025-08-05 01:09:04 +0100

    Delete db_config.php

--- db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Configuração da conexão com o banco de dados
-return [
-    'host' => '127.0.0.1',
-    'user' => 'root',
-    'password' => '', // Substitua pela senha do seu banco de dados
-    'dbname' => 'comissionamento',
-];
-?>
